d798349db861e6671448375baaf9587ab8c1baff
Igor Andrade <andrade.igor@gmail.com>
2015-01-29 00:30:56 -0200

    atualizando spring

---
+++ src/main/java/br/com/audiojus/conf/RestConf.java
@@ -0,0 +1,19 @@
+package br.com.audiojus.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Configuration
+public class RestConf {
+
+	@Bean(name="ObjectMapperCustom")
+	public ObjectMapper getMapper(){
+		ObjectMapper mapper = new ObjectMapper();
+		  mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		  return mapper;
+	}
+	
+}
