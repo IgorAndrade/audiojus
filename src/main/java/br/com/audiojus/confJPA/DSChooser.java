aa0e30ec2e05ef4cf21ff1fde2563073e470c46a
Igor Andrade <andrade.igor@gmail.com>
2015-02-04 20:11:09 -0200

    criando ds de teste e ds de producao

---
+++ src/main/java/br/com/audiojus/confJPA/DSChooser.java
@@ -0,0 +1,39 @@
+package br.com.audiojus.confJPA;
+
+import java.net.UnknownHostException;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DSChooser {
+
+	@Autowired
+	@Qualifier("dataSourceTeste")
+	private DataSource teste;
+	@Autowired
+	@Qualifier("dataSourceProducao")
+	private DataSource producao;
+	@Autowired
+	Environment env;
+
+	// @Autowired
+	// private ServletContext context;
+	// private InetAddress address;
+
+	@Bean(name = "dataSourceEscolhido")
+	public DataSource getDs() {
+		String[] activeProfiles = env.getActiveProfiles();
+		if (activeProfiles == null || activeProfiles.length == 0
+				|| activeProfiles[0].equalsIgnoreCase("producao"))
+			return producao;
+		else
+			return teste;
+	}
+}
