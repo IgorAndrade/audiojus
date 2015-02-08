ea805d8ddbbeb04f6e85d838635e82f5bc580f37
Igor Andrade <andrade.igor@gmail.com>
2015-02-08 00:44:52 -0200

    configurando

--- src/main/java/br/com/audiojus/confJPA/DSChooser.java
+++ src/main/java/br/com/audiojus/confJPA/DSChooser.java
@@ -8,10 +8,11 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class DSChooser {
 
 	@Autowired
